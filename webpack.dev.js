cf26f12d72dc574e4b845f0347052dae8df428e8
lisajoy <lisaharrylock@gmail.com>
2018-05-16 16:08:47 -0700

    copy test files to output

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'cheap-module-source-map',
@@ -13,10 +14,8 @@ module.exports = merge(common, {
     port: 9020
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'IMAGE_VIEWER_SERVICE_URL': JSON.stringify('http://localhost:9021/image-service')
-      }
-    })
+    new CopyWebpackPlugin([
+      {from:'public'} 
+  ]), 
   ]
 });
