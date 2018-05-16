7a6bca9726c534ac6dcb7332bebeff03cfc66282
lisajoy <lisaharrylock@gmail.com>
2018-05-16 16:40:05 -0700

    fix library build

--- webpack.library.js
+++ webpack.library.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/aics-image-viewer/viewer3d/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
