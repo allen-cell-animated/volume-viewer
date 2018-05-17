80243d9b89a777e578a65a7f594c30fdc9e3fc06
lisajoy <lisaharrylock@gmail.com>
2018-05-17 11:03:50 -0700

    remove webpack.common.js and webpack-merge dependency

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,64 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = merge(common, {
-  devtool: 'cheap-module-source-map',
-  output: {
-    publicPath: '/imageviewer/'
-  },
-  devServer: {
-    publicPath: '/imageviewer/',
-    openPage: 'imageviewer/',
-    port: 9020
-  },
-  plugins: [
-    new CopyWebpackPlugin(['public'])
-  ]
-});
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './public/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'imageviewer'),
+        filename: 'image-viewer-ui.bundle.js',
+        publicPath: '/imageviewer/'
+    },
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        publicPath: '/imageviewer/',
+        openPage: 'imageviewer/',
+        port: 9020
+    },
+    plugins: [
+        new CleanWebpackPlugin(['imageviewer']),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new webpack.DefinePlugin({
+            APP_VERSION: JSON.stringify(require("./package.json").version)
+        }),
+        new webpack.ProvidePlugin({
+            d3: 'd3',
+            THREE: 'three',
+            jQuery: 'jquery',
+            $: 'jquery'
+        }),
+        // ignores a webcomponents dependency on a server side module since this is for front end only.
+        // see: https://github.com/webcomponents/webcomponentsjs/issues/794
+        new webpack.IgnorePlugin(/vertx/),
+        new CopyWebpackPlugin(['public'])
+    ],
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'public')
+                ],
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /Worker\.js$/,
+                use: 'worker-loader?inline=true'
+            },
+            {
+                test: /\.html$/,
+                exclude: /index\.html/,
+                use: ['polymer-webpack-loader']
+            }
+        ]
+    }
+};
