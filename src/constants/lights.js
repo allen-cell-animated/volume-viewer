637d3bf5c5f418a2828bac27c3de185746162ce9
dmt <danielt@alleninstitute.org>
2018-05-29 14:11:06 -0700

    bring lighting changes over from prior PR when volume-viewer was integrated with aics-image-viewer-app.

---
+++ src/constants/lights.js
@@ -0,0 +1,46 @@
+export const spotlightSettings = {
+    angle: 6 * THREE.Math.DEG2RAD,
+    castShadow: false,
+    color: 0xffffff,
+    intensity: 0.4,
+    position: {
+        x: -4,
+        y: 3.5,
+        z: 7
+    }
+};
+
+
+export const ambientLightSettings = {
+    color: 0xffffff,
+    intensity: 0.6
+};
+
+export const reflectedLightSettings = {
+    castShadow: false,
+    color: 0xFF88AA,
+    intensity: 0.2,
+    position: {
+        x: 1,
+        y: -5,
+        z: 0
+    }
+};
+
+export const fillLightSettings = {
+    castShadow: false,
+    color: 0xe8d1a9,
+    intensity: 0.15,
+    position: {
+        x: 2.5,
+        y: 0.2,
+        z: 1.5
+    }
+};
+
+export default {
+    spotlightSettings,
+    ambientLightSettings,
+    reflectedLightSettings,
+    fillLightSettings
+};
