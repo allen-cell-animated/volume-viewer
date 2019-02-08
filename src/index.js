2cdd5562defef0ee1a01bc02aaa573fb460ede8e
Dan Toloudis <dmt.reg@gmail.com>
2019-02-08 15:03:40 -0800

    remove VolumeDrawable from public exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import {View3d} from './View3d.js';
 import Volume from './Volume.js';
-import VolumeDrawable from './VolumeDrawable.js';
 import VolumeMaker from './VolumeMaker.js';
 import VolumeLoader from './VolumeLoader.js';
 import {Light, AREA_LIGHT, SKY_LIGHT} from './Light.js';
@@ -8,7 +7,6 @@ import {Light, AREA_LIGHT, SKY_LIGHT} from './Light.js';
 export {
   View3d,
   Volume,
-  VolumeDrawable,
   VolumeMaker,
   VolumeLoader,
   Light,
