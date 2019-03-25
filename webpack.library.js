7590e906e9282e7b6e0d671a91760cf133025276
Dan Toloudis <danielt@alleninstitute.org>
2019-03-25 10:28:35 -0700

    remove babel-polyfill from webpack library config

--- webpack.library.js
+++ webpack.library.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: ['./src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
