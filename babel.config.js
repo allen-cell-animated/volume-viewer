cc775b6e2e8ecc5a5fe38885125c538eb360111d
Dan Toloudis <danielt@alleninstitute.org>
2019-08-19 08:28:46 -0700

    fix inline import of binary assets

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "presets": [
+    "@babel/preset-env",
+    "@babel/preset-typescript"
+  ],
+  "env": {
+    "es": {
+      "presets": [["@babel/preset-env", { "modules": false }]]
+    }
+  },
+  "plugins": [
+    ["babel-plugin-inline-import", {
+      "extensions": [ ".obj" ]
+    }],
+    ["inline-import-data-uri", {
+      "extensions": [ ".png" ]
+    }],
+  ]
+};
