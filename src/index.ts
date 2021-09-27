8b5a6b1feafdebbd2f0657675063978189e51156
Dan <danielt@alleninstitute.org>
2021-09-27 09:50:49 -0700

    convert VolumeLoader

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { RENDERMODE_PATHTRACE, RENDERMODE_RAYMARCH, View3d } from "./View3d";
 import Volume from "./Volume";
 import VolumeMaker from "./VolumeMaker";
-import VolumeLoader from "./VolumeLoader.js";
+import VolumeLoader from "./VolumeLoader";
 import { Light, AREA_LIGHT, SKY_LIGHT } from "./Light";
 
 export {
