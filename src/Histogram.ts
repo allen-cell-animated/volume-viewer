ee4288537c77733a1d45961f5fbd13ef3f701535
Na Hyung Choi <schoinh@gmail.com>
2021-10-01 16:05:07 -0700

    Add comments about ControlPoint and Lut

--- src/Histogram.ts
+++ src/Histogram.ts
@@ -16,16 +16,16 @@ const LUT_ENTRIES = 256;
 const LUT_ARRAY_LENGTH = LUT_ENTRIES * 4;
 
 /**
- * @typedef {Object} ControlPoint
+ * @typedef {Object} ControlPoint Used for the TF (transfer function) editor GUI. Need to be converted to LUT for rendering.
  * @property {number} x The X Coordinate
  * @property {number} opacity The Opacity, from 0 to 1
  * @property {Array.<number>} color The Color, 3 numbers from 0-255 for r,g,b
  */
 
 /**
- * @typedef {Object} Lut
- * @property {Array.<number>} lut LUT_ARRAY_LENGTH element lookup table as array (maps scalar intensity to a rgb color plus alpha)
- * @property {Array.<ControlPoint>} controlPoints
+ * @typedef {Object} Lut Used for rendering.
+ * @property {Array.<number>} lut LUT_ARRAY_LENGTH element lookup table as array (maps scalar intensity to a rgb color plus alpha, with each value from 0-255))
+ * @property {Array.<ControlPoint>} controlPoints 
  */
 type ControlPoint = {
   x: number;
