97630b439f75734be29fb17f8ad71d392c154b46
toloudis <toloudis@users.noreply.github.com>
2022-08-05 20:42:13 +0000

    deploy: 604b0e87a1a9d2ac1499d528fa308dec8d738938

---
+++ src_workers_FetchTiffWorker_ts.volume-viewer-ui.bundle.js
@@ -0,0 +1,420 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/workers/FetchTiffWorker.ts":
+/*!****************************************!*\
+  !*** ./src/workers/FetchTiffWorker.ts ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/asyncToGenerator */ "./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var geotiff__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! geotiff */ "./node_modules/geotiff/dist-module/geotiff.js");
+
+
+
+// from TIFF
+var SAMPLEFORMAT_UINT = 1;
+var SAMPLEFORMAT_INT = 2;
+var SAMPLEFORMAT_IEEEFP = 3;
+
+function castToArray(buf, bytesPerPixel, sampleFormat) {
+  if (sampleFormat === SAMPLEFORMAT_IEEEFP) {
+    if (bytesPerPixel === 4) {
+      return new Float32Array(buf);
+    }
+  } else if (sampleFormat === SAMPLEFORMAT_INT) {
+    if (bytesPerPixel === 1) {
+      return new Int8Array(buf);
+    } else if (bytesPerPixel === 2) {
+      return new Int16Array(buf);
+    } else if (bytesPerPixel === 4) {
+      return new Int32Array(buf);
+    }
+  } else if (sampleFormat === SAMPLEFORMAT_UINT) {
+    if (bytesPerPixel === 1) {
+      return new Uint8Array(buf);
+    } else if (bytesPerPixel === 2) {
+      return new Uint16Array(buf);
+    } else if (bytesPerPixel === 4) {
+      return new Uint32Array(buf);
+    }
+  }
+
+  console.error("TIFF Worker: unsupported sample format ".concat(sampleFormat, " and bytes per pixel ").concat(bytesPerPixel));
+  return new Uint8Array(buf);
+}
+
+self.onmessage = /*#__PURE__*/function () {
+  var _ref = (0,_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0__["default"])( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default().mark(function _callee(e) {
+    var channelIndex, tilesizex, tilesizey, sizez, sizec, dimensionOrder, bytesPerSample, tiff, startindex, incrementz, image, sampleFormat, bytesPerPixel, buffer, u8, imageIndex, zslice, _image, result, arrayresult, offset, src, chmin, chmax, j, val, out, _j, results;
+
+    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default().wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            // TODO index images by time
+            // const time = e.data.time;
+            channelIndex = e.data.channel;
+            tilesizex = e.data.tilesizex;
+            tilesizey = e.data.tilesizey;
+            sizez = e.data.sizez;
+            sizec = e.data.sizec;
+            dimensionOrder = e.data.dimensionOrder;
+            bytesPerSample = e.data.bytesPerSample;
+            _context.next = 9;
+            return (0,geotiff__WEBPACK_IMPORTED_MODULE_2__.fromUrl)(e.data.url);
+
+          case 9:
+            tiff = _context.sent;
+            // load the images of this channel from the tiff
+            // today assume TCZYX so the slices are already in order.
+            startindex = 0;
+            incrementz = 1;
+
+            if (dimensionOrder === "XYZCT") {
+              // we have XYZCT which is the "good" case
+              // TCZYX
+              startindex = sizez * channelIndex;
+              incrementz = 1;
+            } else if (dimensionOrder === "XYCZT") {
+              // we have to loop differently to increment channels
+              // TZCYX
+              startindex = channelIndex;
+              incrementz = sizec;
+            } // huge assumption: planes are in a particular order relative to z and c
+            // get first plane, to get some details about the image
+
+
+            _context.next = 15;
+            return tiff.getImage(startindex);
+
+          case 15:
+            image = _context.sent;
+            // on first image, set up some stuff:
+            sampleFormat = image.getSampleFormat();
+            bytesPerPixel = image.getBytesPerPixel(); // allocate a buffer for one channel
+
+            buffer = new ArrayBuffer(tilesizex * tilesizey * sizez * bytesPerPixel);
+            u8 = new Uint8Array(buffer);
+            imageIndex = startindex, zslice = 0;
+
+          case 21:
+            if (!(zslice < sizez)) {
+              _context.next = 34;
+              break;
+            }
+
+            _context.next = 24;
+            return tiff.getImage(imageIndex);
+
+          case 24:
+            _image = _context.sent;
+            _context.next = 27;
+            return _image.readRasters({
+              width: tilesizex,
+              height: tilesizey
+            });
+
+          case 27:
+            result = _context.sent;
+            arrayresult = Array.isArray(result) ? result[0] : result; // deposit in full channel array in the right place
+
+            offset = zslice * tilesizex * tilesizey;
+
+            if (arrayresult.BYTES_PER_ELEMENT > 4) {
+              console.log("byte size not supported yet");
+            } else if (arrayresult.BYTES_PER_ELEMENT !== bytesPerSample) {
+              console.log("tiff bytes per element mismatch with OME metadata");
+            } else {
+              u8.set(new Uint8Array(arrayresult.buffer), offset * arrayresult.BYTES_PER_ELEMENT);
+            }
+
+          case 31:
+            imageIndex += incrementz, ++zslice;
+            _context.next = 21;
+            break;
+
+          case 34:
+            // all slices collected, now resample to 8 bits full data range
+            src = castToArray(buffer, bytesPerPixel, sampleFormat);
+            chmin = src[0];
+            chmax = src[0];
+
+            for (j = 0; j < src.length; ++j) {
+              val = src[j];
+
+              if (val < chmin) {
+                chmin = val;
+              }
+
+              if (val > chmax) {
+                chmax = val;
+              }
+            }
+
+            out = new Uint8Array(src.length);
+
+            for (_j = 0; _j < src.length; ++_j) {
+              out[_j] = (src[_j] - chmin) / (chmax - chmin) * 255;
+            }
+
+            results = {
+              data: out,
+              channel: channelIndex
+            };
+            postMessage(results, [results.data.buffer]);
+
+          case 42:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+/***/ }),
+
+/***/ "?cdec":
+/*!**********************!*\
+  !*** http (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?753a":
+/*!***********************!*\
+  !*** https (ignored) ***!
+  \***********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?4e4d":
+/*!*********************!*\
+  !*** url (ignored) ***!
+  \*********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?662e":
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/******/ 	// the startup function
+/******/ 	__webpack_require__.x = () => {
+/******/ 		// Load entry module and return exports
+/******/ 		// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 		var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors-node_modules_babel_runtime_regenerator_index_js-node_modules_babel_runtime_helpers_es-31f4f7"], () => (__webpack_require__("./src/workers/FetchTiffWorker.ts")))
+/******/ 		__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 		return __webpack_exports__;
+/******/ 	};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/ensure chunk */
+/******/ 	(() => {
+/******/ 		__webpack_require__.f = {};
+/******/ 		// This file contains only the entry chunk.
+/******/ 		// The chunk loading function for additional chunks
+/******/ 		__webpack_require__.e = (chunkId) => {
+/******/ 			return Promise.all(Object.keys(__webpack_require__.f).reduce((promises, key) => {
+/******/ 				__webpack_require__.f[key](chunkId, promises);
+/******/ 				return promises;
+/******/ 			}, []));
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/get javascript chunk filename */
+/******/ 	(() => {
+/******/ 		// This function allow to reference async chunks and sibling chunks for the entrypoint
+/******/ 		__webpack_require__.u = (chunkId) => {
+/******/ 			// return url for filenames based on template
+/******/ 			return "" + chunkId + ".volume-viewer-ui.bundle.js";
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "/volumeviewer/";
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/importScripts chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded chunks
+/******/ 		// "1" means "already loaded"
+/******/ 		var installedChunks = {
+/******/ 			"src_workers_FetchTiffWorker_ts": 1
+/******/ 		};
+/******/ 		
+/******/ 		// importScripts chunk loading
+/******/ 		var installChunk = (data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			for(var moduleId in moreModules) {
+/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 				}
+/******/ 			}
+/******/ 			if(runtime) runtime(__webpack_require__);
+/******/ 			while(chunkIds.length)
+/******/ 				installedChunks[chunkIds.pop()] = 1;
+/******/ 			parentChunkLoadingFunction(data);
+/******/ 		};
+/******/ 		__webpack_require__.f.i = (chunkId, promises) => {
+/******/ 			// "1" is the signal for "already loaded"
+/******/ 			if(!installedChunks[chunkId]) {
+/******/ 				if(true) { // all chunks have JS
+/******/ 					importScripts(__webpack_require__.p + __webpack_require__.u(chunkId));
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk_aics_volume_viewer"] = self["webpackChunk_aics_volume_viewer"] || [];
+/******/ 		var parentChunkLoadingFunction = chunkLoadingGlobal.push.bind(chunkLoadingGlobal);
+/******/ 		chunkLoadingGlobal.push = installChunk;
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/startup chunk dependencies */
+/******/ 	(() => {
+/******/ 		var next = __webpack_require__.x;
+/******/ 		__webpack_require__.x = () => {
+/******/ 			return __webpack_require__.e("vendors-node_modules_babel_runtime_regenerator_index_js-node_modules_babel_runtime_helpers_es-31f4f7").then(next);
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// run startup
+/******/ 	var __webpack_exports__ = __webpack_require__.x();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=src_workers_FetchTiffWorker_ts.volume-viewer-ui.bundle.js.map
---
+++ src_workers_FetchZarrWorker_ts.volume-viewer-ui.bundle.js
@@ -0,0 +1,278 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/workers/FetchZarrWorker.ts":
+/*!****************************************!*\
+  !*** ./src/workers/FetchZarrWorker.ts ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var zarr__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! zarr */ "./node_modules/zarr/zarr.mjs");
+
+
+function convertChannel(channelData, nx, ny, nz, dtype, downsampleZ) {
+  var nresultpixels = nx * ny * Math.ceil(nz / downsampleZ);
+  var u8 = new Uint8Array(nresultpixels);
+  var xy = nx * ny;
+
+  if (dtype === "|u1") {
+    // flatten the 3d array and convert to uint8
+    // todo test perf with a loop over x,y,z instead
+    for (var z = 0, slice = 0; z < nz; z += downsampleZ, ++slice) {
+      for (var j = 0; j < xy; ++j) {
+        var yrow = Math.floor(j / nx);
+        var xcol = j % nx;
+        u8[j + slice * xy] = channelData[z][yrow][xcol];
+      }
+    }
+  } else {
+    var chmin = 65535; //metadata.channels[i].window.min;
+
+    var chmax = 0; //metadata.channels[i].window.max;
+    // find min and max (only of data we are sampling?)
+
+    for (var _z = 0; _z < nz; _z += downsampleZ) {
+      for (var _j = 0; _j < xy; ++_j) {
+        var _yrow = Math.floor(_j / nx);
+
+        var _xcol = _j % nx;
+
+        var val = channelData[_z][_yrow][_xcol];
+
+        if (val < chmin) {
+          chmin = val;
+        }
+
+        if (val > chmax) {
+          chmax = val;
+        }
+      }
+    } // flatten the 3d array and convert to uint8
+
+
+    for (var _z2 = 0, _slice = 0; _z2 < nz; _z2 += downsampleZ, ++_slice) {
+      for (var _j2 = 0; _j2 < xy; ++_j2) {
+        var _yrow2 = Math.floor(_j2 / nx);
+
+        var _xcol2 = _j2 % nx;
+
+        u8[_j2 + _slice * xy] = (channelData[_z2][_yrow2][_xcol2] - chmin) / (chmax - chmin) * 255;
+      }
+    }
+  }
+
+  return u8;
+}
+
+self.onmessage = function (e) {
+  var time = e.data.time;
+  var channelIndex = e.data.channel;
+  var downsampleZ = e.data.downsampleZ;
+  var store = new zarr__WEBPACK_IMPORTED_MODULE_0__.HTTPStore(e.data.urlStore);
+  (0,zarr__WEBPACK_IMPORTED_MODULE_0__.openArray)({
+    store: store,
+    path: e.data.path,
+    mode: "r"
+  }).then(function (level) {
+    return level.get([time, channelIndex, null, null, null]);
+  }).then(function (channel) {
+    channel = channel;
+    var nz = channel.shape[0];
+    var ny = channel.shape[1];
+    var nx = channel.shape[2];
+    var u8 = convertChannel(channel.data, nx, ny, nz, channel.dtype, downsampleZ);
+    var results = {
+      data: u8,
+      channel: channelIndex
+    };
+    postMessage(results, [results.data.buffer]);
+  });
+};
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/******/ 	// the startup function
+/******/ 	__webpack_require__.x = () => {
+/******/ 		// Load entry module and return exports
+/******/ 		// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 		var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors-node_modules_zarr_zarr_mjs"], () => (__webpack_require__("./src/workers/FetchZarrWorker.ts")))
+/******/ 		__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 		return __webpack_exports__;
+/******/ 	};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/ensure chunk */
+/******/ 	(() => {
+/******/ 		__webpack_require__.f = {};
+/******/ 		// This file contains only the entry chunk.
+/******/ 		// The chunk loading function for additional chunks
+/******/ 		__webpack_require__.e = (chunkId) => {
+/******/ 			return Promise.all(Object.keys(__webpack_require__.f).reduce((promises, key) => {
+/******/ 				__webpack_require__.f[key](chunkId, promises);
+/******/ 				return promises;
+/******/ 			}, []));
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/get javascript chunk filename */
+/******/ 	(() => {
+/******/ 		// This function allow to reference async chunks and sibling chunks for the entrypoint
+/******/ 		__webpack_require__.u = (chunkId) => {
+/******/ 			// return url for filenames based on template
+/******/ 			return "" + chunkId + ".volume-viewer-ui.bundle.js";
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "/volumeviewer/";
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/importScripts chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded chunks
+/******/ 		// "1" means "already loaded"
+/******/ 		var installedChunks = {
+/******/ 			"src_workers_FetchZarrWorker_ts": 1
+/******/ 		};
+/******/ 		
+/******/ 		// importScripts chunk loading
+/******/ 		var installChunk = (data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			for(var moduleId in moreModules) {
+/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 				}
+/******/ 			}
+/******/ 			if(runtime) runtime(__webpack_require__);
+/******/ 			while(chunkIds.length)
+/******/ 				installedChunks[chunkIds.pop()] = 1;
+/******/ 			parentChunkLoadingFunction(data);
+/******/ 		};
+/******/ 		__webpack_require__.f.i = (chunkId, promises) => {
+/******/ 			// "1" is the signal for "already loaded"
+/******/ 			if(!installedChunks[chunkId]) {
+/******/ 				if(true) { // all chunks have JS
+/******/ 					importScripts(__webpack_require__.p + __webpack_require__.u(chunkId));
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk_aics_volume_viewer"] = self["webpackChunk_aics_volume_viewer"] || [];
+/******/ 		var parentChunkLoadingFunction = chunkLoadingGlobal.push.bind(chunkLoadingGlobal);
+/******/ 		chunkLoadingGlobal.push = installChunk;
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/startup chunk dependencies */
+/******/ 	(() => {
+/******/ 		var next = __webpack_require__.x;
+/******/ 		__webpack_require__.x = () => {
+/******/ 			return __webpack_require__.e("vendors-node_modules_zarr_zarr_mjs").then(next);
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// run startup
+/******/ 	var __webpack_exports__ = __webpack_require__.x();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=src_workers_FetchZarrWorker_ts.volume-viewer-ui.bundle.js.map
