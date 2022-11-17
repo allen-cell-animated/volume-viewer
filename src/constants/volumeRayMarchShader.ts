3e91b4bbcf540da318d6b798e329bed109787bb9
Cameron Fraser <cameron.fraser@alleninstitute.org>
2022-11-17 14:58:34 -0800

    break linear and nearest sampling into different functions

--- src/constants/volumeRayMarchShader.ts
+++ src/constants/volumeRayMarchShader.ts
@@ -76,10 +76,10 @@ vec2 offsetFrontBack(float t, float nx, float ny) {
   int a = int(t);
   int ax = int(ATLAS_X);
   vec2 os = vec2(float(a-(a/ax)*ax) / ATLAS_X, float(a/ax) / ATLAS_Y);
-  return os;
+  return clamp(os, vec2(0.0, 0.0), vec2(1.0-1.0/ATLAS_X, 1.0-1.0/ATLAS_Y));
 }
 
-vec4 sampleAs3DTexture(sampler2D tex, vec4 pos) {
+vec4 sampleAtlasLinear(sampler2D tex, vec4 pos) {
   float bounds = float(pos[0] >= 0.0 && pos[0] <= 1.0 &&
                        pos[1] >= 0.0 && pos[1] <= 1.0 &&
                        pos[2] >= 0.0 && pos[2] <= 1.0 );
@@ -92,67 +92,72 @@ vec4 sampleAs3DTexture(sampler2D tex, vec4 pos) {
     (flipVolume.x*(pos.x - 0.5) + 0.5)/ATLAS_X,
     (flipVolume.y*(pos.y - 0.5) + 0.5)/ATLAS_Y);
 
-  if (interpolationEnabled) {
-    // loc ranges from 0 to 1/ATLAS_X, 1/ATLAS_Y
-    // shrink loc0 to within one half edge texel - so as not to sample across edges of tiles.
-    loc0 = vec2(0.5/textureRes.x, 0.5/textureRes.y) + loc0*vec2(1.0-(ATLAS_X)/textureRes.x, 1.0-(ATLAS_Y)/textureRes.y);
-    
-    // interpolate between two slices
-    float z = (pos.z)*(nSlices-1.0);
-    float z0 = floor(z);
-    float t = z-z0; //mod(z, 1.0);
-    float z1 = min(z0+1.0, nSlices-1.0);
+  // loc ranges from 0 to 1/ATLAS_X, 1/ATLAS_Y
+  // shrink loc0 to within one half edge texel - so as not to sample across edges of tiles.
+  loc0 = vec2(0.5/textureRes.x, 0.5/textureRes.y) + loc0*vec2(1.0-(ATLAS_X)/textureRes.x, 1.0-(ATLAS_Y)/textureRes.y);
+  
+  // interpolate between two slices
+  float z = (pos.z)*(nSlices-1.0);
+  float z0 = floor(z);
+  float t = z-z0; //mod(z, 1.0);
+  float z1 = min(z0+1.0, nSlices-1.0);
 
-    // flipped:
-    if (flipVolume.z == -1.0) {
-      z0 = nSlices - z0 - 1.0;
-      z1 = nSlices - z1 - 1.0;
-      t = 1.0 - t;
-    }
-
-    // get slice offsets in texture atlas
-    vec2 o0 = offsetFrontBack(z0,ATLAS_X,ATLAS_Y);//*pix;
-    vec2 o1 = offsetFrontBack(z1,ATLAS_X,ATLAS_Y);//*pix;
-    o0 = clamp(o0, vec2(0.0,0.0), vec2(1.0-1.0/ATLAS_X, 1.0-1.0/ATLAS_Y)) + loc0;
-    o1 = clamp(o1, vec2(0.0,0.0), vec2(1.0-1.0/ATLAS_X, 1.0-1.0/ATLAS_Y)) + loc0;
-
-    vec4 slice0Color = texture2D(tex, o0);
-    vec4 slice1Color = texture2D(tex, o1);
-    // NOTE we could premultiply the mask in the fuse function,
-    // but that is slower to update the maskAlpha value than here in the shader.
-    // it is a memory vs perf tradeoff.  Do users really need to update the maskAlpha at realtime speed?
-    float slice0Mask = texture2D(textureAtlasMask, o0).x;
-    float slice1Mask = texture2D(textureAtlasMask, o1).x;
-    // or use max for conservative 0 or 1 masking?
-    float maskVal = mix(slice0Mask, slice1Mask, t);
-    // take mask from 0..1 to alpha..1
-    maskVal = mix(maskVal, 1.0, maskAlpha);
-    vec4 retval = mix(slice0Color, slice1Color, t);
-    // only mask the rgb, not the alpha(?)
-    retval.rgb *= maskVal;
-    return bounds*retval;
-  } else {
-    // No interpolation - sample just one slice at a pixel center
-    loc0 = floor(loc0 * textureRes) / textureRes;
-    loc0 += vec2(0.5/textureRes.x, 0.5/textureRes.y);
-
-    float z = min(floor(pos.z * nSlices), nSlices-1.0);
-    
-    if (flipVolume.z == -1.0) {
-      z = nSlices - z - 1.0;
-    }
-
-    vec2 o = offsetFrontBack(z, ATLAS_X, ATLAS_Y);
-    o = clamp(o, vec2(0.0, 0.0), vec2(1.0-1.0/ATLAS_X, 1.0-1.0/ATLAS_Y)) + loc0;
-    vec4 voxelColor = texture2D(tex, o);
-
-    // Apply mask
-    float voxelMask = texture2D(textureAtlasMask, o).x;
-    voxelMask = mix(voxelMask, 1.0, maskAlpha);
-    voxelColor.rgb *= voxelMask;
-
-    return bounds*voxelColor;
+  // flipped:
+  if (flipVolume.z == -1.0) {
+    z0 = nSlices - z0 - 1.0;
+    z1 = nSlices - z1 - 1.0;
+    t = 1.0 - t;
   }
+
+  // get slice offsets in texture atlas
+  vec2 o0 = offsetFrontBack(z0,ATLAS_X,ATLAS_Y) + loc0;
+  vec2 o1 = offsetFrontBack(z1,ATLAS_X,ATLAS_Y) + loc0;
+
+  vec4 slice0Color = texture2D(tex, o0);
+  vec4 slice1Color = texture2D(tex, o1);
+  // NOTE we could premultiply the mask in the fuse function,
+  // but that is slower to update the maskAlpha value than here in the shader.
+  // it is a memory vs perf tradeoff.  Do users really need to update the maskAlpha at realtime speed?
+  float slice0Mask = texture2D(textureAtlasMask, o0).x;
+  float slice1Mask = texture2D(textureAtlasMask, o1).x;
+  // or use max for conservative 0 or 1 masking?
+  float maskVal = mix(slice0Mask, slice1Mask, t);
+  // take mask from 0..1 to alpha..1
+  maskVal = mix(maskVal, 1.0, maskAlpha);
+  vec4 retval = mix(slice0Color, slice1Color, t);
+  // only mask the rgb, not the alpha(?)
+  retval.rgb *= maskVal;
+  return bounds*retval;
+}
+
+vec4 sampleAtlasNearest(sampler2D tex, vec4 pos) {
+  float bounds = float(pos[0] >= 0.0 && pos[0] <= 1.0 &&
+                       pos[1] >= 0.0 && pos[1] <= 1.0 &&
+                       pos[2] >= 0.0 && pos[2] <= 1.0 );
+  float nSlices = float(SLICES);
+  vec2 loc0 = vec2(
+    (flipVolume.x*(pos.x - 0.5) + 0.5)/ATLAS_X,
+    (flipVolume.y*(pos.y - 0.5) + 0.5)/ATLAS_Y);
+
+  // No interpolation - sample just one slice at a pixel center
+  loc0 = floor(loc0 * textureRes) / textureRes;
+  loc0 += vec2(0.5/textureRes.x, 0.5/textureRes.y);
+
+  float z = min(floor(pos.z * nSlices), nSlices-1.0);
+  
+  if (flipVolume.z == -1.0) {
+    z = nSlices - z - 1.0;
+  }
+
+  vec2 o = offsetFrontBack(z, ATLAS_X, ATLAS_Y) + loc0;
+  vec4 voxelColor = texture2D(tex, o);
+
+  // Apply mask
+  float voxelMask = texture2D(textureAtlasMask, o).x;
+  voxelMask = mix(voxelMask, 1.0, maskAlpha);
+  voxelColor.rgb *= voxelMask;
+
+  return bounds*voxelColor;
 }
 
 bool intersectBox(in vec3 r_o, in vec3 r_d, in vec3 boxMin, in vec3 boxMax,
@@ -227,7 +232,7 @@ vec4 integrateVolume(vec4 eye_o,vec4 eye_d,
     // AABB clip is independent of this and is only used to determine tnear and tfar.
     pos.xyz = (pos.xyz-(-0.5))/((0.5)-(-0.5)); //0.5 * (pos + 1.0); // map position from [boxMin, boxMax] to [0, 1] coordinates
 
-    vec4 col = sampleAs3DTexture(textureAtlas, pos);
+    vec4 col = interpolationEnabled ? sampleAtlasLinear(textureAtlas, pos) : sampleAtlasNearest(textureAtlas, pos);
 
     if (maxProject != 0) {
       col.xyz *= BRIGHTNESS;
