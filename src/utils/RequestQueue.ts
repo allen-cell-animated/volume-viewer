2c82fcb7d74521c4768bf868c726bef6b86beb7e
Peyton Lee <peyton.lee@alleninstitute.org>
2023-06-26 11:52:49 -0700

    refactor: Changed RequestQueue to use string, explicit stringifier

--- src/utils/RequestQueue.ts
+++ src/utils/RequestQueue.ts
@@ -1,32 +1,34 @@
-import Volume from "../Volume";
-import { IVolumeLoader, LoadSpec } from "../loaders/IVolumeLoader";
-import { StringifyingMap } from "./StringifyingMap";
+import { LoadSpec } from "../loaders/IVolumeLoader";
 
 // TODO: Revive cancelled requests, if the operations being run have not finished yet.
 
-class JSONStringifyingMap<V> extends StringifyingMap<any, V> {
-    protected stringifyKey(key: any): string {
-        return JSON.stringify(key);
-    }
-}
-
-export type RequestItem<T> = {
-    key: any;
-    requestIssuer: () => Promise<T>;
+// Type used when making multiple requests.
+export type Request<K, V> = {
+    key: K;
+    requestAction: () => Promise<V>;
 };
 
-interface QueueItem<T> {
-    // Key used to index this queue item
-    key: any;
-    // this is the actual task and the T is the return type
-    action: () => Promise<T>;
+interface QueueItem<V> {
+    /** Key used to index this queue item. */
+    key: string;
+    /** Action to be run. */
+    action: () => Promise<V>;
+    /** Reference to the promise object that will be resolved when the action is complete. */
+    promise: Promise<V>;
     // value is of promise's type
-    resolve: (value?: T | PromiseLike<T> | undefined) => void;
+    resolve: (value?: V | PromiseLike<V> | undefined) => void;
     // reason is really any
     reject: (reason?: unknown) => void;
+    /** Optional, used to track timeouts when the item will be added to the queue later. */
+    timeoutId?: NodeJS.Timeout;
 }
 
-export default class RequestQueue {
+/**
+ * Manages a queue of asynchronous requests with unique keys, which can be added to or cancelled.
+ * If redundant requests with the same key are issued, the request action will only be run once per key
+ * while the original request is still in the queue.
+ */
+export default class RequestQueue<K> {
     // TODO: Consider limit for number of requests that can be operated on at once
     // TODO: Consider adding staggering for tasks (timeout for how they get added to this ^)
     // VolumeLoader calling .addRequest? -> write a new fake VolumeLoader :)
@@ -34,53 +36,113 @@ export default class RequestQueue {
     //    How does this change VolumeLoader?
     //    How does this integrate with cache?
 
-    /** The maximum number of requests that can be inflight at once.
-    Additional requests will be queued up to run after.*/
+    /** The maximum number of requests that can be handled concurently.
+    Additional requests will be queued up to run when a running request completes.*/
     private maxActiveRequests: number;
 
-    private queue: QueueItem<any>[];
+    private queue: string[];
 
     /** Stores all requests, even those that are currently active. */
-    private pendingRequests: JSONStringifyingMap<Promise<any>>;
+    private allRequests: Map<string, QueueItem<any>>;
     /** Stores requests that are currently processing. */
-    private activeRequests: JSONStringifyingMap<Promise<any>>;
+    private activeRequests: Set<string>;
 
-    constructor(maxActiveRequests = 10) {
-        this.pendingRequests = new JSONStringifyingMap();
-        this.activeRequests = new JSONStringifyingMap();
+    /** Used to convert keys into a string identifier. Can be overridden in constructor. */
+    private keyStringifyFn: (key: K) => string;
+
+    /**
+     * Creates a new RequestQueue.
+     * @param keyStringifyFn Function used to turn the key type into a string identifier. By default, uses JSON.stringify.
+     * @param maxActiveRequests The maximum number of requests that will be handled concurrently. This is 10 by default.
+     */
+    constructor(keyStringifyFn = (key: K) => JSON.stringify(key), maxActiveRequests = 10) {
+        this.allRequests = new Map();
+        this.activeRequests = new Set();
         this.queue = [];
         this.maxActiveRequests = maxActiveRequests;
+        this.keyStringifyFn = keyStringifyFn;
+    }
+
+    /**
+     * Stores request metadata to the internal map of all pending requests.
+     * @param keyString string identifier of the request.
+     * @param requestAction callable function action of the request.
+     * @returns the promise of the request, to be resolved later.
+     */
+    private registerRequest<T>(keyString: string, requestAction: () => Promise<T>): Promise<T> {
+        // Create a new promise and store the resolve and reject callbacks for later.
+        // This lets us perform the actual action at a later point, when the request is ready to be
+        // processed.
+        let promiseResolve, promiseReject;
+        const promise = new Promise<T>((resolve, reject) => {
+            promiseResolve = resolve;
+            promiseReject = reject;
+        });
+        // Store the request data.
+        this.allRequests.set(keyString, {
+            key: keyString,
+            action: requestAction,
+            resolve: promiseResolve,
+            reject: promiseReject,
+            promise,
+        });
+        return promise;
     }
 
     /**
      * Adds a request with a unique key to the queue, if it doesn't already exist.
      * @param key The key used to track the request. Keys will be compared using JSON.stringify(). 
-     * @param requestIssuer Function that should be called to complete the request. The function
-     * will be called only once per unique key, and may be deferred at any time.
-     * @returns A promise that will resolve on completion of the request.
-     * TODO: Does this fail if you use the same key for different return types?
+     * @param requestAction Function that will be called to complete the request. The function
+     *  will be run only once per unique key while the request exists, and may be deferred by the
+     *  queue at any time.
+     * @returns A promise that will resolve on completion of the request. If multiple requests are issued
+     *  with the same key, the same promise will be returned until request is resolved or cancelled.
      */
-    public async addRequest<T>(key: any, requestIssuer: () => Promise<T>): Promise<T> {
-        if (!this.pendingRequests.has(key)) {  // First time we are adding this request, so do loadAction
-            // Create a new promise, deferring the request action until 
-            const promise = new Promise<T>((resolve, reject) => {
-                this.queue.push({
-                    key,
-                    action: requestIssuer,
-                    resolve,
-                    reject
-                });
-                this.dequeue();  // Check to see if we can run a task.
-            })
-            // Store the request promise.
-            this.pendingRequests.set(key, promise);
+    public addRequest<T>(key: K, requestAction: () => Promise<T>): Promise<T> {
+        const keyString = this.keyStringifyFn(key);
+        if (!this.allRequests.has(keyString)) {  // New request!
+            const promise = this.registerRequest(keyString, requestAction);
+            this.queue.push(keyString);
+            this.dequeue();  // Check if we can run a task.
         }
-        return this.pendingRequests.get(key);
+        // TODO: Possible type error if addRequest is called with the same key on two different issuer return types?
+        return this.allRequests.get(keyString)?.promise!;
     }
 
-    public async addRequests<T>(requests: RequestItem<T>[], delayMs = 10) {
-        let delay = 0;
+    /**
+     * Adds multiple requests to the queue, with an optional delay between each.
+     * @param requests An array of RequestItems, which include a key and a request action.
+     * @param delayMs An optional delay in milliseconds to be added between each request.
+     *  For example, a delay of 10 ms will cause the second request to be added to the queue
+     *  after 10 ms, the third to added after 20 ms, and so on. Set to 10 ms by default. 
+     * @returns 
+     */
+    public addRequests<T>(requests: Request<K, T>[], delayMs = 10): Promise<T>[] {
+        const promises: Promise<T>[] = [];
         // TODO
+        for (let i = 0; i < requests.length; i++) {
+            const item = requests[i];
+            const keyString = this.keyStringifyFn(item.key);
+            if (this.allRequests.has(keyString)) {
+                // Existing request
+                promises.push(this.allRequests.get(keyString)?.promise!);
+            } else {
+                // Add this request to our map of all requests, but don't add to queue until after delay.
+                const promise = this.registerRequest(keyString, item.requestAction);
+                promises.push(promise);
+                const timeoutId = setTimeout(() => {
+                    this.queue.push(keyString);
+                    this.dequeue();  // Check if we can run a task.
+                }, delayMs * i);
+                // Store the timeoutId to the request metadata in case we need to cancel this task later.
+                const requestData = this.allRequests.get(keyString)!;
+                requestData.timeoutId = timeoutId;
+            }
+            promises.push(
+                
+            );
+        }
+        return promises;
     }
 
     /**
@@ -89,29 +151,40 @@ export default class RequestQueue {
      * requests already active.
      */
     private dequeue(): boolean {
-        if (this.activeRequests.size() >= this.maxActiveRequests) {
+        if (this.activeRequests.values.length >= this.maxActiveRequests
+            || this.queue.length === 0) {
             return false;
         }
-        const requestItem = this.queue.shift();
-        if (!requestItem){
-            return false;  // Can't clean up request data because it doesn't exist.
+        const requestKey = this.queue.shift();
+        if (!requestKey){
+            return false;
         }
+        const requestItem = this.allRequests.get(requestKey);
+        if (!requestItem) {
+            return false;
+        }
+
+        const keyString = requestItem.key;
         try {
             // Mark that this request is active
-            const promise = this.pendingRequests.get(requestItem)!;
-            this.activeRequests.set(requestItem.key, promise);
+            this.activeRequests.add(keyString);
 
             // Run the task
             requestItem.action().then((value) => {
                 requestItem.resolve(value);
                 // Clean up the item
-                this.activeRequests.delete(requestItem.key);
+                this.activeRequests.delete(keyString);
+                this.allRequests.delete(keyString);
                 this.dequeue();
             }).catch((err) => {
+                this.activeRequests.delete(keyString);
+                this.allRequests.delete(keyString);
                 requestItem.reject(err);
                 this.dequeue();
             })
         } catch (err) {
+            this.activeRequests.delete(keyString);
+            this.allRequests.delete(keyString);
             requestItem.reject(err);
             this.dequeue();
         }
