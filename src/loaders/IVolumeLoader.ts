a05ba2ee0bd851c001c83662066379480e736557
Cameron Fraser <cameron.fraser@alleninstitute.org>
2023-09-28 14:32:21 -0700

    update `loadSpecToString`

--- src/loaders/IVolumeLoader.ts
+++ src/loaders/IVolumeLoader.ts
@@ -13,12 +13,12 @@ export class LoadSpec {
 
 export function loadSpecToString(spec: LoadSpec): string {
   const { min, max } = spec.subregion;
-  return `${spec.scene}:${spec.time}:x(${min.x},${max.x}):y(${min.y},${max.y}):z(${min.z},${max.z})`;
+  return `${spec.scene}:${spec.multiscaleLevel}:${spec.time}:x(${min.x},${max.x}):y(${min.y},${max.y}):z(${min.z},${max.z})`;
 }
 
 export class VolumeDims {
   subpath = "";
-  // shape: [t, c, z, y, x]n
+  // shape: [t, c, z, y, x]
   shape: number[] = [0, 0, 0, 0, 0];
   // spacing: [t, c, z, y, x]; generally expect 1 for non-spatial dimensions
   spacing: number[] = [1, 1, 1, 1, 1];
