4d0d7ad5dcb1f42fc34a80a473ca7891ee02bbe0
Cameron Fraser <cameron.fraser@alleninstitute.org>
2023-10-30 12:32:02 -0700

    simplify `createVolumeLoader`; add `concurrencyLimit`

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -15,33 +15,29 @@ export type CreateLoaderOptions = {
   fileType?: VolumeFileFormat;
   cache?: VolumeCache;
   scene?: number;
+  concurrencyLimit?: number;
 };
-
-const forceString = (value: string | string[]): string => (typeof value === "object" ? value[0] : value);
 
 export async function createVolumeLoader(
   path: string | string[],
-  options: CreateLoaderOptions
+  options?: CreateLoaderOptions
 ): Promise<IVolumeLoader> {
-  if (options.fileType) {
-    switch (options.fileType) {
-      case VolumeFileFormat.ZARR:
-        return await OMEZarrLoader.createLoader(forceString(path), options.scene, options.cache);
-      case VolumeFileFormat.JSON:
-        return new JsonImageInfoLoader(path, options.cache);
-      case VolumeFileFormat.TIFF:
-        return new TiffLoader(forceString(path));
-      default:
-        throw new Error(`Unknown file type: ${options.fileType}`);
-    }
-  } else {
-    const pathString = forceString(path);
-    if (pathString.endsWith(".json")) {
-      return new JsonImageInfoLoader(path, options.cache);
-    } else if (pathString.endsWith(".tif") || pathString.endsWith(".tiff")) {
-      return new TiffLoader(pathString);
-    } else {
+  const pathString = typeof path === "object" ? path[0] : path;
+
+  switch (options?.fileType) {
+    case VolumeFileFormat.ZARR:
       return await OMEZarrLoader.createLoader(pathString, options.scene, options.cache);
-    }
+    case VolumeFileFormat.JSON:
+      return new JsonImageInfoLoader(path, options.cache);
+    case VolumeFileFormat.TIFF:
+      return new TiffLoader(pathString);
+    default:
+      if (pathString.endsWith(".json")) {
+        return new JsonImageInfoLoader(path, options?.cache);
+      } else if (pathString.endsWith(".tif") || pathString.endsWith(".tiff")) {
+        return new TiffLoader(pathString);
+      } else {
+        return await OMEZarrLoader.createLoader(pathString, options?.scene, options?.cache, options?.concurrencyLimit);
+      }
   }
 }
