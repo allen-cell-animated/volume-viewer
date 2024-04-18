2e9a5ddd05d9c727f0046959bb73f4fef3d6de1f
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-04-17 22:53:23 -0700

    simplify sci notation function

--- src/utils/num_utils.ts
+++ src/utils/num_utils.ts
@@ -1,13 +1,9 @@
+const DEFAULT_SIG_FIGS = 12;
+
 // Adapted from https://gist.github.com/ArneS/2ecfbe4a9d7072ac56c0.
 function digitToUnicodeSupercript(n: number): string {
   const subst = [0x2070, 185, 178, 179, 0x2074, 0x2075, 0x2076, 0x2077, 0x2078, 0x2079];
   return String.fromCharCode(subst[n]);
-}
-
-function numberToUnicodeSuperscript(input: number): string {
-  const prefix = input < 0 ? "⁻" : "";
-  const digits = Math.abs(input).toString().split("");
-  return prefix + digits.map((digit) => digitToUnicodeSupercript(parseInt(digit, 10))).join("");
 }
 
 /**
@@ -25,28 +21,26 @@ function numberToUnicodeSuperscript(input: number): string {
  * numberToSciNotation(1400, 3) // "1.40×10³"
  * ```
  */
-function numberToSciNotation(input: number, significantFigures: number): string {
-  significantFigures = Math.max(significantFigures, 1);
-  const prefix = input < 0 ? "-" : "";
-  input = Math.abs(input);
-  // Apply precision in case it causes input to round up to the next power of 10.
-  // For example, if input = 0.99 and significantFigures = 1, we want to round to 1 now.
-  // Otherwise we'd get `exponent = -1` and 10×10⁻¹ instead of 1×10⁰.
-  input = Number.parseFloat(input.toPrecision(significantFigures));
-  if (input === 0) {
-    return "0×10⁰";
-  }
-  const exponent = Math.floor(Math.log10(input));
-  const coefficient = input / 10 ** exponent;
-  return `${prefix}${coefficient.toFixed(significantFigures - 1)}×10${numberToUnicodeSuperscript(exponent)}`;
+function numberToSciNotation(input: number, sigFigs = DEFAULT_SIG_FIGS): string {
+  const expString = input.toExponential(sigFigs - 1);
+  const [coefficient, exponent] = expString.split("e");
+  const expSign = exponent[0] === "-" ? "⁻" : "";
+  const expSuperscript = exponent
+    .slice(1)
+    .split("")
+    .map((digit) => digitToUnicodeSupercript(Number(digit)))
+    .join("");
+  return `${coefficient}×10${expSign}${expSuperscript}`;
 }
 
-// export function formatMaxSigFigs(input: number, significantFigures: number): string {
-//   return numberToSciNotation(input, significantFigures);
-// }
+const ZERO_SCI = numberToSciNotation(0);
 
 export function formatNumber(value: number): string {
   if (value < 0.01 || value > 10_000) {
+    const sciNotation = numberToSciNotation(value);
+    if (sciNotation === ZERO_SCI) {
+      return "0";
+    }
     return numberToSciNotation(value, 3);
   } else if (value < 1) {
     return value.toPrecision(1);
