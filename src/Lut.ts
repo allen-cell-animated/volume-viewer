a629f9a9c801d8071c1bc2cb3b4cd2d5513846cb
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-07-12 15:00:27 -0700

    Revert "factor out creating full range lut too"
    This reverts commit 96375f8793e5ca1dc2ae4f5a761c05d2e3514dee.

--- src/Lut.ts
+++ src/Lut.ts
@@ -83,20 +83,6 @@ const createFullRangeControlPoints = (opacityMin = 0, opacityMax = 1): [ControlP
   { x: 255, opacity: opacityMax, color: [255, 255, 255] },
 ];
 
-function createFullRangeLut(): Uint8Array {
-  const lut = new Uint8Array(LUT_ARRAY_LENGTH);
-
-  // simple linear mapping for actual range
-  for (let x = 0; x < lut.length / 4; ++x) {
-    lut[x * 4 + 0] = 255;
-    lut[x * 4 + 1] = 255;
-    lut[x * 4 + 2] = 255;
-    lut[x * 4 + 3] = x;
-  }
-
-  return lut;
-}
-
 /**
  * @typedef {Object} Lut Used for rendering.
  * @property {Array.<number>} lut LUT_ARRAY_LENGTH element lookup table as array
@@ -206,7 +192,17 @@ export class Lut {
 
   // basically, the identity LUT with respect to opacity
   createFullRange(): Lut {
-    this.lut = createFullRangeLut();
+    const lut = new Uint8Array(LUT_ARRAY_LENGTH);
+
+    // simple linear mapping for actual range
+    for (let x = 0; x < lut.length / 4; ++x) {
+      lut[x * 4 + 0] = 255;
+      lut[x * 4 + 1] = 255;
+      lut[x * 4 + 2] = 255;
+      lut[x * 4 + 3] = x;
+    }
+
+    this.lut = lut;
     this.controlPoints = createFullRangeControlPoints();
     return this;
   }
