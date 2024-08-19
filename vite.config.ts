0233ff1685f913da8bd3161aaeb50a9f657a7d94
DMT <danielt@alleninstitute.org>
2024-08-18 21:22:58 -0700

    start to work on vite library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+import { globSync } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import glob from "glob";
@@ -13,7 +16,27 @@ export default defineConfig({
       'es/test/*',
     ]
   },
-  build: {
-
+  build: { 
+    lib: { entry: resolve(__dirname, 'src/index.ts'), formats: ['es'] },   
+    rollupOptions: {
+      input: Object.fromEntries(
+        globSync('src/**/*.js').map(file => [
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          path.relative(
+            'src',
+            file.slice(0, file.length - path.extname(file).length)
+          ),
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      output: {
+        format: 'es',
+        dir: 'dist'
+      }
+        }    
   },
+  resolve: { alias: { src: resolve('src/') } },
 });
