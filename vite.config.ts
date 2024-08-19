ff0a5295bc5d173cfc0a7b488d8821fd37aea0fe
dmt <dmt@completelydifferent.com>
2024-08-19 08:08:13 -0700

    autoformat and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,33 @@
-import { globSync } from 'glob';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { globSync } from "glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import glob from "glob";
-import { configDefaults } from 'vitest/config'
-
-const tests = glob.sync(resolve(__dirname, "src/**/test/**/*.{js,ts}"));
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   test: {
-    exclude:[
-      ...configDefaults.exclude,
-      'es/test/*',
-    ]
+    exclude: [...configDefaults.exclude, "es/test/*"],
   },
-  build: { 
-    lib: { entry: resolve(__dirname, 'src/index.ts'), formats: ['es'] },   
+  build: {
+    lib: { entry: resolve(__dirname, "src/index.ts"), formats: ["es"] },
     rollupOptions: {
       input: Object.fromEntries(
-        globSync('src/**/*.js').map(file => [
+        globSync("src/**/*.js").map((file) => [
           // This remove `src/` as well as the file extension from each
           // file, so e.g. src/nested/foo.js becomes nested/foo
-          path.relative(
-            'src',
-            file.slice(0, file.length - path.extname(file).length)
-          ),
+          path.relative("src", file.slice(0, file.length - path.extname(file).length)),
           // This expands the relative paths to absolute paths, so e.g.
           // src/nested/foo becomes /project/src/nested/foo.js
-          fileURLToPath(new URL(file, import.meta.url))
+          fileURLToPath(new URL(file, import.meta.url)),
         ])
       ),
       output: {
-        format: 'es',
-        dir: 'dist'
-      }
-        }    
+        format: "es",
+        dir: "dist",
+      },
+    },
   },
-  resolve: { alias: { src: resolve('src/') } },
+  resolve: { alias: { src: resolve("src/") } },
 });
