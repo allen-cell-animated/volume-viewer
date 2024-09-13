60544d044a5a463d7627b4dcc80ec9084d554503
toloudis <toloudis@users.noreply.github.com>
2024-09-13 00:27:23 +0000

    deploy: 4a6dc8000b11a3348ff2848e7972fcc0f2d59228

--- src_workers_FetchTiffWorker_ts.volume-viewer-ui.bundle.js
+++ src_workers_FetchTiffWorker_ts.volume-viewer-ui.bundle.js
@@ -385,7 +385,7 @@ self.onmessage = async e => {
 /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
 /******/ 		var document = __webpack_require__.g.document;
 /******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
 /******/ 				scriptUrl = document.currentScript.src;
 /******/ 			if (!scriptUrl) {
 /******/ 				var scripts = document.getElementsByTagName("script");
