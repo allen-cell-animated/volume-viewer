1814d6ccbbc338ad7e9e1df7ebf97bd083606130
Peyton Lee <peyton.lee@alleninstitute.org>
2024-12-13 16:24:54 -0800

    Revert "refactor: Remove `dataMinBin` and `dataMaxBin` in Histogram"
    This reverts commit 1b3416b3e3cc1aa78f655b7a13d67a322fc7ac7e.

--- src/Histogram.ts
+++ src/Histogram.ts
@@ -21,12 +21,18 @@ export default class Histogram {
   private min: number;
   /** Max value in the original raw data. */
   private max: number;
-  /** Size of each histogram bin in the scale of the original data. */
+  /** Size of each histogram bin in the scale of the original raw data. */
   private binSize: number;
+  /** Index of the first nonzero bin with at least 1 sample */
+  private dataMinBin: number;
+  /** Index of the last nonzero bin with at least 1 sample */
+  private dataMaxBin: number;
   private pixelCount: number;
   public maxBin: number;
 
   constructor(data: TypedArray<NumberType>) {
+    this.dataMinBin = 0;
+    this.dataMaxBin = 0;
     this.maxBin = 0;
     this.bins = new Uint32Array();
     this.min = 0;
@@ -39,6 +45,20 @@ export default class Histogram {
     this.min = hinfo.min;
     this.max = hinfo.max;
     this.binSize = hinfo.binSize;
+
+    // track the first and last nonzero bins with at least 1 sample
+    for (let i = 1; i < this.bins.length; i++) {
+      if (this.bins[i] > 0) {
+        this.dataMinBin = i;
+        break;
+      }
+    }
+    for (let i = this.bins.length - 1; i >= 1; i--) {
+      if (this.bins[i] > 0) {
+        this.dataMaxBin = i;
+        break;
+      }
+    }
 
     this.pixelCount = data.length;
 
@@ -85,19 +105,21 @@ export default class Histogram {
   }
 
   /**
-   * Returns the minimum bin index.
+   * Returns the minimum bin index with a non-zero value.
    * @return {number}
    */
   getMin(): number {
-    return 0;
+    // Note that this will currently always return 0, unless NBINS changes.
+    return this.dataMinBin;
   }
 
   /**
-   * Returns the maximum bin index.
+   * Returns the maximum bin index with a non-zero value.
    * @return {number}
    */
   getMax(): number {
-    return NBINS - 1;
+    // Note that this will always return 255, unless NBINS changes.
+    return this.dataMaxBin;
   }
 
   getNumBins(): number {
